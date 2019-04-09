2736745efec69ec0128f14942797970533d276d1
Marko Petric <marko.petric@cern.ch>
2019-04-09 14:44:02 +0200

    test

--- .podio-ci.d/compile_and_test.sh
+++ .podio-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ cd /podio
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install .. && \
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 .. && \
 make -j4 && \
 make test && \
 make install
